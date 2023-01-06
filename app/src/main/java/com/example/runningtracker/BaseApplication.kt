4ffe32c3b4dae6354e3b93938d10c3fc72370ef5
dkap937-209 <67822005+dkap937-209@users.noreply.github.com>
2023-01-06 15:11:15 +1300

    Dagger hilt dependencies setup

--- app/src/main/java/com/example/runningtracker/BaseApplication.kt
+++ app/src/main/java/com/example/runningtracker/BaseApplication.kt
@@ -1,4 +1,8 @@
 package com.example.runningtracker
 
-class BaseApplication {
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication: Application() {
 }
--- app/src/main/java/com/example/runningtracker/MainActivity.kt
+++ app/src/main/java/com/example/runningtracker/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.runningtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.runningtracker.db.RunDAO
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint //This is causing problems
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var runDAO: RunDAO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        Log.d("runDAo", "RUN DAO: ${runDAO.hashCode()}")
+
     }
 }
