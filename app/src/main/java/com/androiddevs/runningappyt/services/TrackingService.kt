0605ccf7a6cf7e694c906e3f77131957a9a3afd0
dkap937-209 <67822005+dkap937-209@users.noreply.github.com>
2023-01-29 22:17:12 +1300

    Implemented functionality to cancel a run

--- app/src/main/java/com/androiddevs/runningappyt/services/TrackingService.kt
+++ app/src/main/java/com/androiddevs/runningappyt/services/TrackingService.kt
@@ -56,6 +56,7 @@ typealias Polylines = MutableList<Polyline>
 class TrackingService : LifecycleService() {
 
     private var isFirstRun = true
+    var serviceKilled = false
 
     @Inject
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient
@@ -90,6 +91,15 @@ class TrackingService : LifecycleService() {
         })
     }
 
+    private fun killService(){
+        serviceKilled = true
+        isFirstRun = true
+        pauseService()
+        postInitialValues()
+        stopForeground(true)
+        stopSelf()
+    }
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let{
             when(it.action){
@@ -111,6 +121,7 @@ class TrackingService : LifecycleService() {
                 }
                 ACTION_STOP_SERVICE->{
                     Timber.d("Stopped service")
+                    killService()
                 }
             }
         }
@@ -179,10 +190,13 @@ class TrackingService : LifecycleService() {
             set(curNotificationBuilder, ArrayList<NotificationCompat.Action>())
         }
 
-        curNotificationBuilder = baseNotificationBuilder
-            .addAction(R.drawable.ic_pause_black_24dp, notificationActionText, pendingIntent)
+        if(!serviceKilled){
+            curNotificationBuilder = baseNotificationBuilder
+                .addAction(R.drawable.ic_pause_black_24dp, notificationActionText, pendingIntent)
 
-        notificationManager.notify(NOTIFICATION_ID, curNotificationBuilder.build())
+            notificationManager.notify(NOTIFICATION_ID, curNotificationBuilder.build())
+        }
+
     }
 
     @SuppressLint("MissingPermission")
@@ -257,10 +271,12 @@ class TrackingService : LifecycleService() {
         startForeground(NOTIFICATION_ID, baseNotificationBuilder.build())
 
         timeRunInSeconds.observe(this, Observer{
-            val notification = curNotificationBuilder
-                .setContentText(TrackingUtility.getFormattedStopWatchTime(it*1000L))
+            if(!serviceKilled){
+                val notification = curNotificationBuilder
+                    .setContentText(TrackingUtility.getFormattedStopWatchTime(it*1000L))
 
-            notificationManager.notify(NOTIFICATION_ID, notification.build())
+                notificationManager.notify(NOTIFICATION_ID, notification.build())
+            }
         })
     }
 
